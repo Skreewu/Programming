7e9358016091d66f34ab38fedc892aad882d3775
Александр Рыгалов <AVRygalov@gmail.com>
2024-10-25 17:48:09 +0700

    feat: add class Order

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Order.cs
@@ -0,0 +1,61 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Order
+    {
+        private static IdGenerator idGenerator = new IdGenerator();
+        public int Id {  get; private set; }
+        public DateTime OrderCreationDate { get; }
+        private Address _adress;
+        private List<Item> _items;
+        private double _amount;
+
+        public Address Address
+        {
+            get 
+            { 
+                return _adress; 
+            }
+            set 
+            { 
+                _adress = value; 
+            }
+        }
+        public List<Item> Items
+        {
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+            }
+        }
+        public double Amount
+        {
+            get
+            {
+                if (_items == null || _items.Count == 0) return 0;
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    _amount += _items[i].Cost;
+                }
+                return _amount;
+            }
+        }
+        public Order()
+        {
+            Id = idGenerator.GetNextId();
+            OrderCreationDate = DateTime.Now;
+            Items = new List<Item>();
+            Address = new Address();
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿enum OrderStatus
+{
+    New,
+    Processing,
+    Assembly,
+    Sent,
+    Delivered,
+    Returned,
+    Abandoned
+}
