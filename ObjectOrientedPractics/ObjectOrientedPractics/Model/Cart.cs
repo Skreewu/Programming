e27574b5b6d8286359f73a556bdb3d5259183dd9
Александр Рыгалов <AVRygalov@gmail.com>
2024-10-25 17:35:31 +0700

    feat: add class Customer

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Cart
+    {
+        private List<Item> _items;
+        private double _amount;
+
+        public List<Item> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+        public double Amount
+        {
+            get
+            {
+                if (_items == null || _items.Count == 0) return 0;
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    _amount += _items[i].Cost;
+                }
+                return _amount;
+            }
+        }
+        public Cart()
+        {
+            List<Item> items = new List<Item>();
+        }
+    }
+}
