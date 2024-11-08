25c58bf143a26325c78940c1d0989956cad50575
Александр Рыгалов <AVRygalov@gmail.com>
2024-11-08 21:46:15 +0700

    feat: add classes "PercentDiscount" and "PointDiscount"

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PercentDiscount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class PercentDiscount
+    {
+        private const int maxDiscountPercentage = 10;
+
+        public double TotalSpent { get; private set; }
+        public Category Category { get; private set; }
+        public int CurrentDiscountPercentage { get; private set; }
+
+        /// <summary>
+        /// Возвращает информацию о скидке.
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                return $"Процентная '{Category}' - {CurrentDiscountPercentage}%";
+            }
+        }
+
+        public PercentDiscount(Category category)
+        {
+            Category = category;
+            CurrentDiscountPercentage = 1;
+            TotalSpent = 0;
+        }
+
+        public void Update(List<Item> items)
+        {
+            TotalSpent = CalculateTotalCost(items);
+            CurrentDiscountPercentage = Math.Min(1 + (int)(TotalSpent / 1000), maxDiscountPercentage);
+        }
+        public double Calculate(List<Item> items)
+        {
+            double discountAmount = 0;
+            discountAmount = CalculateTotalCost(items) * CurrentDiscountPercentage / 100;
+            
+            return discountAmount;
+        }
+
+        public double Apply(List<Item> items)
+        {
+            return CalculateTotalCost(items) - Calculate(items);
+        }
+        /// <summary>
+        /// Считает стоимость всего списка товаров.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Возвращает стоимость.</returns>
+        private double CalculateTotalCost(List<Item> items)
+        {
+            double totalCost = 0;
+            foreach (var item in items)
+            {
+                if (item.Category == Category)
+                {
+                    totalCost += item.Cost;
+                }
+            }
+            return totalCost;
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PointsDiscount.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о скидке.
+    /// </summary>
+    internal class PointsDiscount
+    {
+        /// <summary>
+        /// Хранит данные о накопительных баллах.
+        /// </summary>
+        private int _points;
+
+        /// <summary>
+        /// Задает и возвращает накопительные баллы.
+        /// </summary>
+        public int Points
+        {
+            get
+            {
+                return _points;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Value must be positive");
+                }
+                _points = value;
+            }
+        }
+        public string Info
+        {
+            get { return $"Накопительная – {Points} баллов"; }
+        }
+        public PointsDiscount() { }
+        /// <summary>
+        /// Расчитывает размер скидки.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Возвращает размер скидки.</returns>
+        public double Calculate(List<Item> items)
+        {
+            double maxDiscount = CalculateTotalCost(items) * 0.30;
+            double discount = Math.Min(Points, maxDiscount);
+
+            return discount;
+        }
+        /// <summary>
+        /// Применяет скидку, списывая баллы.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Возвращает размер скидки.</returns>
+        public double Apply(List<Item> items)
+        {
+            double discount = Calculate(items);
+            Points -= (int)discount;
+
+            return CalculateTotalCost(items) - discount;
+        }
+        /// <summary>
+        /// Начисляет баллы после покупки.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        public void Update(List<Item> items)
+        {
+            int earnedPoints = (int)Math.Ceiling(CalculateTotalCost(items) * 0.10);
+            Points += earnedPoints;
+        }
+        /// <summary>
+        /// Считает стоимость всего списка товаров.
+        /// </summary>
+        /// <param name="items">Список товаров.</param>
+        /// <returns>Возвращает стоимость.</returns>
+        private double CalculateTotalCost(List<Item> items)
+        {
+            double totalCost = 0;
+            foreach (var item in items)
+            {
+                totalCost += item.Cost;
+            }
+            return totalCost;
+        }
+    }
+}
