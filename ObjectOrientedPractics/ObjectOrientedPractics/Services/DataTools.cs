34206eaf12997a61580f673812cb6e9882dadcd1
Александр Рыгалов <AVRygalov@gmail.com>
2024-11-18 19:47:41 +0700

    feat: add filter in DataTools

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Services/DataTools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientedPractics.Model;
+
+namespace ObjectOrientedPractics.Services
+{
+    internal static class DataTools
+    {
+        public static List<Item> Filter(List<Item> items, Func<Item, bool> compare)
+        {
+            List<Item> newItems = new List<Item>();
+            foreach (Item item in items)
+            {
+                if (compare(item))
+                {
+                    newItems.Add(item);
+                }
+            }
+            return newItems;
+        }
+    }
+}
