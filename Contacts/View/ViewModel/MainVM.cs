b40fd72ae67346c187ce2d6a12c6832385822acc
Александр Рыгалов <AVRygalov@gmail.com>
2025-03-16 18:11:28 +0700

    features: add the ViewModel class for MainWindow.xaml

--- Contacts/View/ViewModel/MainVM.cs
+++ Contacts/View/ViewModel/MainVM.cs
@@ -8,8 +8,58 @@ using View.Model;
 
 namespace View.ViewModel
 {
-    internal class MainVM
+    internal class MainVM : INotifyPropertyChanged
     {
-        
+        private Contact _contact;
+
+        public MainVM()
+        {
+            _contact = new Contact();
+        }
+        public string Name
+        {
+            get => _contact.Name;
+            set
+            {
+                if (_contact.Name != value)
+                {
+                    _contact.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _contact.PhoneNumber;
+            set
+            {
+                if (_contact.PhoneNumber != value)
+                {
+                    _contact.PhoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _contact.Email;
+            set
+            {
+                if (_contact.Email != value)
+                {
+                    _contact.Email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
